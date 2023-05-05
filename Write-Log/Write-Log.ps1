7744dd0e5fafa9bd13ae7676bab4b8e12153f0eb
mic05 <it@deltaoffice.de>
2023-05-05 20:30:45 +0200

    fixes #2

--- Write-Log/Write-Log.ps1
+++ Write-Log/Write-Log.ps1
@@ -9,7 +9,11 @@ function Write-Log {
     )
     
     if (!([system.diagnostics.eventlog]::SourceExists($Source))) {
-        New-EventLog -LogName $Logname -Source $Source
+        if((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            New-EventLog -LogName $Logname -Source $Source
+        } else {
+            Write-Host "Source doesn't exist and can't be created without admin rights." -ForegroundColor Red
+        }
     }
     Write-EventLog -LogName $Logname -Source $Source -EntryType $Type -EventId $EventId -Message $Message
 }
